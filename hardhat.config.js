ce33cfe98c7e32763bb1d5ea2e1a4626af3af51d
ihor-protsenko-theraven <ihor.protsenko@theraven.tech>
2022-01-12 15:44:20 +0200

    add config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
-const privateKey = process.env.PRIVATE_KEY;
+const privateKey = "3eca296b2d3220209f65f8e549154fa1c423ab75ee0167b44a1dc8f0a8b99868";
 const polygonscanApikey = "IN194AG23PKQFB2VIEJMR7KQUHGXDXDUHJ";
 const maticvigilAppId = "fd0ba0c297cf03e21d0db62c4b1afc17b473d9f8";
 module.exports = {
