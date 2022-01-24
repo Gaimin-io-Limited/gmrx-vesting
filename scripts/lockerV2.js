8c326c40d8373ea0c5cd5b8bab869d89a454b53f
ihor-protsenko-theraven <ihor.protsenko@theraven.tech>
2022-01-24 15:32:36 +0200

    added case

--- scripts/lockerV2.js
+++ scripts/lockerV2.js
@@ -12,24 +12,22 @@ async function main() {
     const contractFactory = await hre.ethers.getContractFactory("TimeLockedWalletFactory");
     const factoryAddress = "0x07b1e78Ea789FD6755b6C62D23301FC7074be6b0";
     const contract = contractFactory.attach(factoryAddress);
-    console.log(list)
 
 
         for (const item of list) {
             let firstUnlockTime = Math.floor(new Date().getTime() / 1000) + 3 * 60;
          await token.approve(factoryAddress, item.amount);
          console.log("approved");
-         let result = await contract.newTimeLockedWallet(item.wallet.toString(), item.amount, 7, firstUnlockTime, 1 * 60);
+         let result = await contract.newTimeLockedWallet(item.wallet.toString(), item.amount, item.numberOfPeriods, firstUnlockTime, 1 * 60);
          console.log("done: ", result);
-         // console.log(item.wallet, item.amount, item.numberOfPeriods.toString(),item.firstUnlockTime.toString(), item.periodDuration.toString())
      }
 
 }
 
 function locker(){
-    const startSalaryTime = 1642776919;
+    const startSalaryTime = Math.floor(new Date().getTime() / 1000)
     const amountMultiplier = '000000000000000000';
-    const unlockEquity = startSalaryTime + (2678400*6);
+    const unlockEquity = startSalaryTime + (2678400*6); //2592000 30 day
     const unlockSeed = startSalaryTime + (2678400*2);
     const unlockStrategic =  startSalaryTime + (86400*21);
     const unlnockPrivate =  startSalaryTime + (86400*14);
@@ -40,7 +38,7 @@ function locker(){
     const unlnockDev =  startSalaryTime + (2678400*6);
     const unlnockTreasury =  startSalaryTime + 2678400;
     const unlnockTeam =  startSalaryTime + (2678400*12);
-    const periodDuration = 172800000;
+    const periodDuration = 86400;
 
     let amount;
     let wallet;
@@ -53,58 +51,45 @@ function locker(){
     wallet = item.Wallet;
     numberOfPeriods = item.Period * 30;
 
-
-
-        if ( item.Group === 'Equity') {
-            firstUnlockTime = unlockEquity;
-        }
-
-        if (item.Group === 'Seed') {
-            firstUnlockTime = unlockSeed;
-        }
-
-        if (item.Group === 'Strategic') {
-            firstUnlockTime = unlockStrategic;
-        }
-
-        if ( item.Group === 'Private') {
-            firstUnlockTime = unlnockPrivate
-        }
-
-        if (item.Group === 'Public') {
-            firstUnlockTime = unlnockPublic;
-        }
-
-        if (item.Group === 'Corporate') {
-            firstUnlockTime = unlnockCorporate;
-        }
-
-        if (item.Group === 'NFT') {
-            firstUnlockTime = unlnockNftGD;
-        }
-
-        if (item.Group === 'Community') {
-            firstUnlockTime = unlnockCommunity;
-        }
-
-        if (item.Group === 'Development') {
-            firstUnlockTime = unlnockDev;
-        }
-
-        if (item.Group === 'Treasury') {
-            firstUnlockTime = unlnockTreasury;
-        }
-
-        if (item.Group === 'Team') {
-            firstUnlockTime = unlnockTeam
-        }
-
-
-        // console.log( "\x1b[1m","parsed:","✔",pizda)
+     switch (item.Group) {
+         case 'Equity':
+             firstUnlockTime = unlockEquity;
+             break
+         case 'Seed' :
+             firstUnlockTime = unlockSeed;
+             break
+         case 'Strategic' :
+             firstUnlockTime = unlockStrategic;
+             break
+         case  'Private' :
+             firstUnlockTime = unlnockPrivate
+             break
+         case 'Public' :
+             firstUnlockTime = unlnockPublic;
+             break
+         case 'Corporate' :
+             firstUnlockTime = unlnockCorporate;
+             break
+         case 'NFT':
+             firstUnlockTime = unlnockNftGD;
+             break
+         case  'Community' :
+             firstUnlockTime = unlnockCommunity;
+             break
+         case 'Development' :
+             firstUnlockTime = unlnockDev;
+             break
+         case 'Treasury' :
+             firstUnlockTime = unlnockTreasury;
+             break
+         case 'Team' :
+             firstUnlockTime = unlnockTeam
+             break
+     }
         return {wallet, amount, numberOfPeriods,firstUnlockTime,periodDuration}
 
 })
-    // console.log(list)
+
 }
 
 
