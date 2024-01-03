6429225a2327a2d693faec50ae1c953abc779421
Vova Yatsyk <vova@theraven.tech>
2024-01-03 14:38:09 +0200

    use latest solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,7 +32,7 @@ module.exports = {
         },
     },
     solidity: {
-        version: "0.8.20",
+        version: "0.8.23",
         settings: {
             optimizer: {
                 enabled: false,
