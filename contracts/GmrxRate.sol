44af9f78e15456b2b9518967c70996753ba7476b
WilliamVoitsekhovskyi <viliam2323@gmail.com>
2024-05-01 15:10:58 +0300

    clean up

--- contracts/GmrxRate.sol
+++ contracts/GmrxRate.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GmrxRate is Ownable{
 
